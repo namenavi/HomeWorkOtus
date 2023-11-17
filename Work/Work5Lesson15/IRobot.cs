19b8e890af66ab9e1aecb8461aa509233c593c6f
Ivan <name.navi96@gmail.com>
2023-11-17 23:22:45 +0300

    ДЗ №5

---
+++ Work/Work5Lesson15/IRobot.cs
@@ -0,0 +1,26 @@
+﻿/*
+ В этом ДЗ вы научитесь явному вызову интерфейсов, их наследованию и реализации методов по умолчанию.
+
+
+Описание/Пошаговая инструкция выполнения домашнего задания:
+Создать интерфейс IRobot с публичным методами string GetInfo() и List GetComponents(), а также string GetRobotType() с дефолтной реализацией, возвращающей значение "I am a simple robot.".
+Создать интерфейс IChargeable с методами void Charge() и string GetInfo().
+Создать интерфейс IFlyingRobot как наследник IRobot с дефолтной реализацией GetRobotType(), возвращающей строку "I am a flying robot.".
+Создать класс Quadcopter, наследующий IFlyingRobot и IChargeable. В нём создать список компонентов List _components = new List {"rotor1","rotor2","rotor3","rotor4"} и возвращать его из метода GetComponents().
+Реализовать метод Charge() должен писать в консоль "Charging..." и через 3 секунды "Charged!". Ожидание в 3 секунды реализовать через Thread.Sleep(3000).
+Реализовать все методы интерфейсов в классе. До этого пункта достаточно было "throw new NotImplementedException();"
+В чат напишите также время, которое вам потребовалось для реализации домашнего задания.
+  */
+
+namespace Work5Lesson15
+{
+    public interface IRobot
+    {
+        public string GetInfo();
+        public List<string> GetComponents();
+        public string GetRobotType()
+        {
+            return "I am a flying robot.";
+        }
+    }
+}
---
+++ Work/Work5Lesson15/Quadcopter.cs
@@ -0,0 +1,53 @@
+﻿/*
+ В этом ДЗ вы научитесь явному вызову интерфейсов, их наследованию и реализации методов по умолчанию.
+
+
+Описание/Пошаговая инструкция выполнения домашнего задания:
+Создать интерфейс IRobot с публичным методами string GetInfo() и List GetComponents(), а также string GetRobotType() с дефолтной реализацией, возвращающей значение "I am a simple robot.".
+Создать интерфейс IChargeable с методами void Charge() и string GetInfo().
+Создать интерфейс IFlyingRobot как наследник IRobot с дефолтной реализацией GetRobotType(), возвращающей строку "I am a flying robot.".
+Создать класс Quadcopter, наследующий IFlyingRobot и IChargeable. В нём создать список компонентов List _components = new List {"rotor1","rotor2","rotor3","rotor4"}
+и возвращать его из метода GetComponents().
+Реализовать метод Charge() должен писать в консоль "Charging..." и через 3 секунды "Charged!". Ожидание в 3 секунды реализовать через Thread.Sleep(3000).
+Реализовать все методы интерфейсов в классе. До этого пункта достаточно было "throw new NotImplementedException();"
+В чат напишите также время, которое вам потребовалось для реализации домашнего задания.
+  */
+
+namespace Work5Lesson15
+{
+    public class Quadcopter : IFlyingRobot, IChargeable
+    {
+        List<string> _components = new List<string> { "rotor1", "rotor2", "rotor3", "rotor4" };
+        public void Charge()
+        {
+            Console.WriteLine("Charging...");
+            Thread.Sleep(3000);
+            Console.WriteLine("Charged!");
+        }
+
+        public List<string> GetComponents()
+        {
+            if(_components == null)
+              throw new ArgumentNullException("Не объявлен лист");
+            return _components;
+        }
+
+         string IRobot.GetInfo()
+        {
+            return $"""
+                      Версия выполняющейся в данный момент программы: {typeof(Program).Assembly.GetName().Version},
+                      Дата создания выполняющейся в данный момент программы: {File.GetCreationTime(AppDomain.CurrentDomain.BaseDirectory +
+                      System.Diagnostics.Process.GetCurrentProcess().ProcessName + ".exe")}
+                """;
+        }
+
+        string IChargeable.GetInfo()
+        {
+            return $"""
+                      Версия выполняющейся в данный момент программы: {typeof(Program).Assembly.GetName().Version},
+                      Дата создания выполняющейся в данный момент программы: {File.GetCreationTime(AppDomain.CurrentDomain.BaseDirectory + 
+                      System.Diagnostics.Process.GetCurrentProcess().ProcessName + ".exe")}
+                """;
+        }
+    }
+}
