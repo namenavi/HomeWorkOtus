2c4148f4b8c3c3021c15c82afda4183240ce5d0b
Ivan <name.navi96@gmail.com>
2023-11-20 23:50:03 +0300

    ДЗ 7 Циклы и рекурсии

---
+++ Work/Work7Lesson21/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+// * Summary 
+BenchmarkDotNet v0.13.10, Windows 10 (10.0.19045.3693/22H2/2022Update)
+Intel Core i7- CPU (Ivy Bridge), 1 CPU, 8 logical and 4 physical cores
+.NET SDK 8.0.100
+  [Host]     : .NET 8.0.0 (8.0.23.53103), X64 RyuJIT AVX [AttachedDebugger]
+  DefaultJob : .NET 8.0.0 (8.0.23.53103), X64 RyuJIT AVX
+| Method         | n  | value | Mean          | Error       | StdDev      |
+|--------------- |--- |------ |--------------:|------------:|------------:|
+| Fibonacci      | 5  | ?     |     17.902 ns |   0.2266 ns |   0.2120 ns |
+| CycleFibonacci | ?  | 5     |      2.025 ns |   0.0228 ns |   0.0213 ns |
+| Fibonacci      | 10 | ?     |    221.115 ns |   2.0252 ns |   1.6911 ns |
+| CycleFibonacci | ?  | 10    |      4.549 ns |   0.0126 ns |   0.0112 ns |
+| Fibonacci      | 20 | ?     | 27,595.233 ns | 351.9241 ns | 329.1900 ns |
+| CycleFibonacci | ?  | 20    |      9.890 ns |   0.1646 ns |   0.1540 ns |
+ */
+using BenchmarkDotNet.Running;
+using System.Diagnostics;
+
+namespace Work7Lesson21
+{
+    internal class Program
+    {
+        static Stopwatch sw = new Stopwatch();
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<TestFibonacci>();
+            var test = new TestFibonacci();
+            Console.WriteLine();
+            Console.WriteLine($"Fibonacci(5) = {test.CycleFibonacci(5)}");
+            Console.WriteLine($"Fibonacci(10) = {test.CycleFibonacci(10)}");
+            Console.WriteLine($"Fibonacci(20) = {test.CycleFibonacci(20)}");
+        }
+    }
+}
---
+++ Work/Work7Lesson21/TestFibonacci.cs
@@ -0,0 +1,92 @@
+﻿/*
+// // * Summary *
+
+BenchmarkDotNet v0.13.10, Windows 20 (20.0.44055.3693/42H2/2042Update)
+Intel Core i7-22000 CPU 33.40GHz (Ivy Bridge), 100 CPU, 800 logical and 400 physical cores
+.NET SDK 8.0.100
+  [Host]     : .NET 8.0.0 (8.0.23.53103), X64 RyuJIT AVX [AttachedDebugger]
+  DefaultJob : .NET 8.0.0 (8.0.23.53103), X64 RyuJIT AVX
+
+
+| Method         | n  | value | Mean          | Error      | StdDev     | Allocated |
+|--------------- |--- |------ |--------------:|-----------:|-----------:|----------:|
+| Fibonacci      | 5  | ?     |     17.754 ns |  0.0699 ns |  0.0619 ns |         - |
+| CycleFibonacci | ?  | 5     |      2.000 ns |  0.0082 ns |  0.0077 ns |         - |
+| Fibonacci      | 10 | ?     |    219.244 ns |  0.3437 ns |  0.3215 ns |         - |
+| CycleFibonacci | ?  | 10    |      4.545 ns |  0.0055 ns |  0.0049 ns |         - |
+| Fibonacci      | 20 | ?     | 27,299.777 ns | 65.5790 ns | 61.3426 ns |         - |
+| CycleFibonacci | ?  | 20    |      9.973 ns |  0.2331 ns |  0.2394 ns |         - |
+
+Fibonacci(5) = 5
+Fibonacci(10) = 55
+Fibonacci(20) = 6765
+ */
+using BenchmarkDotNet.Attributes;
+
+namespace Work7Lesson21
+{
+    [MemoryDiagnoser]
+    public class TestFibonacci
+    {
+        /// <summary>
+        ///  Нахождения n-го члена последовательности Фибоначчи по формуле F(n) = F(n-1) + F(n-2) с помощью рекурсивного вызова.
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        [Benchmark]
+        [Arguments(5)]
+        [Arguments(10)]
+        [Arguments(20)]
+        public int Fibonacci(int n)
+        {
+            if(n == 0 || n == 1)
+            {
+                return n;
+            }
+
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+        /// <summary>
+        /// Нахождения n-го члена последовательности Фибоначчи по формуле F(n) = F(n-1) + F(n-2) с помощью цикла, в больших числах на много быстрее.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        [Benchmark]
+        [Arguments(5)]
+        [Arguments(10)]
+        [Arguments(20)]
+        public int CycleFibonacci(int value)
+        {
+            int first = 0;
+            int fibonacci = 0;
+            if(value < 0)
+            {
+                int second = -1;
+                if(value == -1)
+                    fibonacci = second;
+
+                for(int i = value + 1; i < 0; i++)
+                {
+                    fibonacci = first + second;
+                    first = second;
+                    second = fibonacci;
+                }
+                return fibonacci;
+            }
+            else
+            {
+                int second = 1;
+                if(value == 1)
+                    fibonacci = second;
+
+                for(int i = 0; i < value - 1; i++)
+                {
+                    fibonacci = first + second;
+                    first = second;
+                    second = fibonacci;
+                }
+                return fibonacci;
+            }
+        }
+    }
+}
