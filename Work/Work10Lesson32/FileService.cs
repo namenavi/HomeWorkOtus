2903d64040150dd4011c281714af13c1f4b846cc
Ivan <name.navi96@gmail.com>
2023-12-16 14:33:58 +0300

    Переделано дз10 с учетом замечаний

---
+++ Work/Work10Lesson32/FileService.cs
@@ -0,0 +1,85 @@
+﻿namespace Work10Lesson32
+{
+    public static class FileService
+    {
+        /// <summary>
+        /// Запись файлов
+        /// </summary>
+        /// <param name="dir"></param>
+        /// <param name="dir2"></param>
+        /// <returns></returns>
+        public static async Task WriteFile(DirectoryInfo dir, DirectoryInfo dir2)
+        {
+            try
+            {
+                string filename;
+                for(int i = 1; i <= 10; i++)
+                {
+                    filename = $"file{i}.txt";
+                    //создание и перезапись файла
+                    using(StreamWriter writer = new StreamWriter($"{dir}\\{filename}", false))
+                    {
+                        await writer.WriteAsync(filename);
+                    }
+                    // добавление в файл
+                    using(StreamWriter writer = new StreamWriter($"{dir}\\{filename}", true))
+                    {
+                        await writer.WriteAsync("\t" + DateTime.Now);
+                    }
+                }
+
+                for(int i = 1; i <= 10; i++)
+                {
+                    filename = $"file{i}.txt";
+                    using(StreamWriter writer = new StreamWriter($"{dir2}\\{filename}", false))
+                    {
+                        await writer.WriteAsync(filename);
+                    }
+                    // добавление в файл
+                    using(StreamWriter writer = new StreamWriter($"{dir2}\\{filename}", true))
+                    {
+                        await writer.WriteAsync("\t" + DateTime.Now);
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        /// <summary>
+        /// Читение файлов
+        /// </summary>
+        /// <param name="dir"></param>
+        /// <param name="dir2"></param>
+        /// <returns></returns>
+        public static async Task ReadFile(DirectoryInfo dir, DirectoryInfo dir2)
+        {
+            try
+            {
+                for(int i = 1; i <= 10; i++)
+                {
+                    using(StreamReader reader = new StreamReader($"{dir}\\file{i}.txt"))
+                    {
+                        string text = await reader.ReadToEndAsync();
+                        Console.WriteLine($"Текст файла file{i}.txt: {text} + дополнение");
+                    }
+                }
+                Console.WriteLine();
+
+                for(int i = 1; i <= 10; i++)
+                {
+                    using(StreamReader reader = new StreamReader($"{dir2}\\file{i}.txt"))
+                    {
+                        string text = await reader.ReadToEndAsync();
+                        Console.WriteLine($"Текст файла file{i}.txt: {text} + дополнение");
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
--- Work/Work10Lesson32/Program.cs
+++ Work/Work10Lesson32/Program.cs
@@ -1,89 +1,23 @@
-﻿using System.Text;
-using System.Xml.Linq;
-using static System.Net.Mime.MediaTypeNames;
-
-namespace Work10Lesson32
+﻿namespace Work10Lesson32
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private const string _directory = "C:\\Otus\\TestDir1";
+        private const string _directory2 = "C:\\Otus\\TestDir2";
+        private static void Main(string[] args)
         {
-            var dir = new DirectoryInfo("C:\\Otus\\TestDir1");
-            var dir2 = new DirectoryInfo("C:\\Otus\\TestDir2");
+            var dir = new DirectoryInfo(_directory);
+            var dir2 = new DirectoryInfo(_directory2);
 
             if(!dir.Exists)
                 dir.Create();
+
             if(!dir2.Exists)
                 dir2.Create();
-            WriteFile(dir, dir2);
-            ReadFile(dir, dir2);
+
+            FileService.WriteFile(dir, dir2).Wait();
+            FileService.ReadFile(dir, dir2).Wait();
             Console.ReadLine();
-        }
-        /// <summary>
-        /// Запись файлов
-        /// </summary>
-        /// <param name="dir"></param>
-        /// <param name="dir2"></param>
-        /// <returns></returns>
-        static async Task WriteFile(DirectoryInfo dir, DirectoryInfo dir2)
-        {
-            string filename;
-            for(int i = 1; i <= 10; i++)
-            {
-                filename = $"file{i}.txt";
-                using(FileStream fstream1 = File.Create($"{dir}\\{filename}"))
-                {
-                    byte[] buffer = Encoding.UTF8.GetBytes(filename + "\t" + DateTime.Now);//fstream.name нифига не нейм, а путь стрима
-                    await fstream1.WriteAsync(buffer, 0, buffer.Length);
-                }
-            }
-
-            for(int i = 1; i <= 10; i++)
-            {
-                filename = $"file{i}.txt";
-                using(FileStream fstream2 = File.Create($"{dir2}\\{filename}"))
-                {
-                    byte[] buffer = Encoding.UTF8.GetBytes(filename + "\t" + DateTime.Now);
-                    await fstream2.WriteAsync(buffer, 0, buffer.Length);
-
-                }
-            }
-          
-        }
-        /// <summary>
-        /// Читение файлов
-        /// </summary>
-        /// <param name="dir"></param>
-        /// <param name="dir2"></param>
-        /// <returns></returns>
-        static async Task ReadFile(DirectoryInfo dir, DirectoryInfo dir2)
-        {
-            for(int i = 1; i <= 10; i++)
-            {
-                using(FileStream fileStream = new FileStream($"{dir}\\file{i}.txt", FileMode.Open))
-                {
-                    // выделяем массив для считывания данных из файла
-                    byte[] buffer = new byte[fileStream.Length];
-                    // считываем данные
-                    await fileStream.ReadAsync(buffer, 0, buffer.Length);
-                    string TextFromFile = Encoding.UTF8.GetString(buffer);
-                    Console.WriteLine($"Текст файла file{i}.txt: {TextFromFile} + дополнение");
-                }
-            }
-            Console.WriteLine();
-
-            for(int i = 1; i <= 10; i++)
-            {
-                using(FileStream fileStream = new FileStream($"{dir2}\\file{i}.txt", FileMode.Open))
-                {
-                    // выделяем массив для считывания данных из файла
-                    byte[] buffer = new byte[fileStream.Length];
-                    // считываем данные
-                    await fileStream.ReadAsync(buffer, 0, buffer.Length);
-                    string TextFromFile = Encoding.UTF8.GetString(buffer);
-                    Console.WriteLine($"Текст файла file{i}.txt: {TextFromFile} + дополнение");
-                }
-            }
         }
     }
 }
