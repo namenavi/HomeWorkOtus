e80e871877ba001575cfcd6474781a225ad16481
Ivan <name.navi96@gmail.com>
2023-12-27 23:46:45 +0300

    добавлены исправления в ДЗ13

---
+++ Work/Work13Lesson40/EnumerableExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Work13Lesson40
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<T> Top<T>(this IEnumerable<T> list, double porcent) where T : IComparable<T>
+        {
+            if(1d >= porcent || porcent >= 100d)
+                throw new ArgumentException("Переданное значение больше 100% или меньше 1%");
+            var procentItem = (double)list.Count() * porcent / 100;
+            var elementCountToProc = (int)Math.Ceiling(procentItem);
+            return list.OrderByDescending(x => x).Take(elementCountToProc);
+        }
+        public static IEnumerable<T> Top<T, TKey>(this IEnumerable<T> list, double porcent, Func<T, TKey> keySelector) where T : IComparable<T>
+        {
+            return list.Top(porcent).OrderByDescending(keySelector);
+        }
+    }
+}
--- Work/Work13Lesson40/Person.cs
+++ Work/Work13Lesson40/Person.cs
@@ -1,4 +1,21 @@
-﻿public class Person
+﻿public class Person : IComparable<Person>
 {
     public int Age { get; set; }
+    public int CompareTo(Person other)
+    {
+        if(other == null)
+            return 1;
+        if(Age < other.Age)
+        {
+            return -1;
+        }
+        else if(Age > other.Age)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
 }
