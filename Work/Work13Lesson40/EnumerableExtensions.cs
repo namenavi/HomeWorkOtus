5fb2de286022fbc96ab5d8064d505b0a8931ed7d
Ivan <name.navi96@gmail.com>
2024-01-08 23:23:30 +0300

    Улучшено дз 13. Убрана необходимость IComparable<Т> для реализации произвольных классов

--- Work/Work13Lesson40/EnumerableExtensions.cs
+++ Work/Work13Lesson40/EnumerableExtensions.cs
@@ -2,17 +2,40 @@
 {
     public static class EnumerableExtensions
     {
+        /// <summary>
+        /// Метод расширение принимающий значение Х от 1 до 100 и 
+        /// возвращающий заданное количество процентов от выборки 
+        /// с округлением количества элементов в большую сторону.
+        /// </summary>
+        /// <typeparam name="T">Должен реализовать IComparable<T></typeparam>
+        /// <param name="list">Коллекция для сравнения</param>
+        /// <param name="porcent">КОлличество процентов элементов для возврата</param>
+        /// <returns></returns>
         public static IEnumerable<T> Top<T>(this IEnumerable<T> list, double porcent) where T : IComparable<T>
         {
-            if(1d >= porcent || porcent >= 100d)
+            return list.Top(porcent, x => x);
+        }
+        /// <summary>
+        /// Метод расширение принимающий значение Х от 1 до 100 и 
+        /// возвращающий заданное количество процентов от выборки 
+        /// с округлением количества элементов в большую сторону, который принимает ещё и поле
+        /// , по которому будут отбираться топ Х элементов.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TKey">Должен реализовать IComparable<T></typeparam>
+        /// <param name="list">Коллекция для сравнения</param>
+        /// <param name="porcent">Колличество процентов элементов для возврата</param>
+        /// <param name="keySelector">Функция сортировки</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static IEnumerable<T> Top<T, TKey>(this IEnumerable<T> list, double porcent, Func<T, TKey> keySelector) where TKey : IComparable<TKey>
+        {
+            if(1d > porcent || porcent > 100d)
                 throw new ArgumentException("Переданное значение больше 100% или меньше 1%");
             var procentItem = (double)list.Count() * porcent / 100;
             var elementCountToProc = (int)Math.Ceiling(procentItem);
-            return list.OrderByDescending(x => x).Take(elementCountToProc);
-        }
-        public static IEnumerable<T> Top<T, TKey>(this IEnumerable<T> list, double porcent, Func<T, TKey> keySelector) where T : IComparable<T>
-        {
-            return list.Top(porcent).OrderByDescending(keySelector);
+            return list.OrderByDescending(keySelector)
+                       .Take(elementCountToProc);
         }
     }
 }
--- Work/Work13Lesson40/Person.cs
+++ Work/Work13Lesson40/Person.cs
@@ -1,21 +1,4 @@
-﻿public class Person : IComparable<Person>
+﻿public class Person
 {
     public int Age { get; set; }
-    public int CompareTo(Person other)
-    {
-        if(other == null)
-            return 1;
-        if(Age < other.Age)
-        {
-            return -1;
-        }
-        else if(Age > other.Age)
-        {
-            return 1;
-        }
-        else
-        {
-            return 0;
-        }
-    }
 }
