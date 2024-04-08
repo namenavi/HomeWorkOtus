80a5007ce7c91e23eb82b848aba8742bceb89263
Ivan <name.navi96@gmail.com>
2024-04-08 23:56:01 +0300

    Add Class

---
+++ Work/Work16Lesson47/Models/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace Work16Lesson47.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FistName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ Work/Work16Lesson47/Models/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace Work16Lesson47.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int CustomerId { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
